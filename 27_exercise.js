ab1c170a1f032f96079e8530a0f85781b178a598
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2023-08-06 15:24:58 +0200

    27 task is done

---
+++ 27_exercise.js
@@ -0,0 +1,7 @@
+let reverseArray =arr=>{
+
+    return arr.reverse();
+}
+console.log(reverseArray([1,2,3,4,5]));
+console.log(reverseArray([6,7,8,9,10]));
+console.log(reverseArray([11,22,33,44,55]));
