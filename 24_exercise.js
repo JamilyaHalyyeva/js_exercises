4b540b2d4704fd257ff8a7685ea0ffbd70946e50
JamilyaHalyyeva <jamilya.halyyeva@gmail.com>
2023-08-06 15:38:22 +0200

    24 task is done

---
+++ 24_exercise.js
@@ -0,0 +1,7 @@
+let reverseWithCase=str=>{
+
+    return str.split('').reverse().join('');
+}
+console.log(reverseWithCase('Hello wordl'));
+console.log(reverseWithCase('Hello'));
+console.log(reverseWithCase('Hello teacher'));
